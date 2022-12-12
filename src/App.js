f4d110264fd732abcbd914b2d0439a48a1dbfd8c
Ahmetozcan28 <mrkronoss@hotmail.com>
2022-12-12 14:34:13 +0300

    fix: fix

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
   const [items, setItems] = useState([
@@ -7,25 +7,35 @@ function App() {
       value: "",
     },
   ]);
-  const pasteHandle = (e) => {
+  useEffect(() => {
+    if (items.length === 0) {
+      setItems([{ key: "", value: "" }])
+    }
+  }, [items])
+  const pasteHandle = (e, index) => {
     e.preventDefault();
     const pastedData = e.clipboardData.getData("text");
     if (pastedData) {
       const arr = pastedData.split("\n").map((text) => {
-          let [key, value] = text.split("=");
-          return { key, value };
-        }).filter(Boolean);
+        let [key, value] = text.split("=");
+        return { key, value };
+      }).filter(Boolean);
+      if (arr.length > 0) {
+        setItems(items => [...items.slice(0, index), ...arr, ...items.slice(index + 1)]
+        )
+      }
       console.log(arr);
     }
   };
+
   return (
     <div className="h-[100vh] bg-black">
       <div className="container mx-auto py-4">
         <div className="grid gap-y-4">
           {items.map((item, index) => (
-            <div className="grid grid-cols-2 gap-x-4">
+            <div className="flex gap-x-4">
               <input
-                onPaste={pasteHandle}
+                onPaste={e => pasteHandle(e, index)}
                 onChange={(e) => {
                   setItems((items) =>
                     items.map((item, i) => {
@@ -37,7 +47,7 @@ function App() {
                   );
                 }}
                 placeholder="Örn: API_URL"
-                className="h-10 placeholder:text-white/50 rounded bg-white/5 border border-white/20 text-sm px-3 text-white outline-none"
+                className="flex-1 h-10 placeholder:text-white/50 rounded bg-white/5 border border-white/20 text-sm px-3 text-white outline-none"
                 type={"text"}
                 value={item.key}
               ></input>
@@ -52,10 +62,13 @@ function App() {
                     })
                   );
                 }}
-                className="h-10 rounded bg-white/5 border border-white/20 text-sm px-3 text-white outline-none"
+                className="flex-1 h-10 rounded bg-white/5 border border-white/20 text-sm px-3 text-white outline-none"
                 type={"text"}
                 value={item.value}
               ></input>
+              <button
+                onClick={() => setItems(items => items.filter((_, key) => key !== index))}
+                className="h-10 w-10 bg-red-500 text-white text-sm rounded-md	">x</button>
             </div>
           ))}
         </div>
